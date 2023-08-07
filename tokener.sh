2f17e094c0cdc2eb2cba51edaea4952e005c6fe1
Enjeru <spirizeon@proton.me>
2023-08-07 16:39:11 +0530

    removed .sh script

--- tokener.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "enter your openai API key:"
-read KEY1
-echo "openai='${KEY1}'" > .env 
-echo "enter your discord bot token:"
-read KEY2
-echo "TOKEN='${KEY2}'" >> .env
-echo "successfully added!"
